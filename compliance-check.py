fa881ddc9eef11a3a6db692e27b268860929ffb2
Lars Buntemeyer <lars.buntemeyer@hereon.de>
2025-05-25 13:06:46 +0000

    add cc6 check

--- compliance-check.py
+++ compliance-check.py
@@ -58,10 +58,10 @@ def compliance_check(catalog_filename):
 
     print(f"checking {len(files)} datasets")
     # Run cf and adcc checks
-    path = files
+    path = files[50:60]
     #'/mnt/CORDEX_CMIP6_tmp/sim_data/CORDEX/CMIP6/DD/EUR-12/GERICS/ERA5/evaluation/r1i1p1f1/REMO2020/v1-r1/mon/tas/v20241120/tas_EUR-12_ERA5_evaluation_r1i1p1f1_GERICS_REMO2020_v1-r1_mon_197901-198812.nc'
     # path = "/mnt/CORDEX_CMIP6_tmp/aux_data/cordex-cmip5/CORDEX/output/EUR-11/DMI/ECMWF-ERAINT/evaluation/r1i1p1/HIRHAM5/v1/fx/orog/v20140620/orog_EUR-11_ECMWF-ERAINT_evaluation_r1i1p1_DMI-HIRHAM5_v1_fx.nc"
-    checker_names = ["cf:1.9"]
+    checker_names = ["cf:1.9", "cc6"]
     verbose = 1
     criteria = "normal"
     output_filename = report_filename
@@ -205,7 +205,7 @@ def main():
     non_empty_errors = get_non_empty_errors(cc_data)
     for k, v in non_empty_errors.items():
         log.error(f"{k}: {v}")
-        log_issues_from_errors(non_empty_errors)
+        # log_issues_from_errors(non_empty_errors)
     return non_empty_errors
 
 
